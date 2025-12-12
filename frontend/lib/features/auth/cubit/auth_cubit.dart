bf6299e514f66c9281a1299d83e4cbe6b4817dc1
Samuel Ketema <samuelketema01@gmail.com>
2025-12-12 21:03:46 +0300

    offline implementation

--- frontend/lib/features/auth/cubit/auth_cubit.dart
+++ frontend/lib/features/auth/cubit/auth_cubit.dart
@@ -1,16 +1,19 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:taskapp/core/services/sp_service.dart';
+import 'package:taskapp/features/auth/repositories/auth_local_repository.dart';
 import 'package:taskapp/features/auth/repositories/auth_remote_repository.dart';
 import 'package:taskapp/models/user_model.dart';
 part 'auth_state.dart';
 
 class AuthCubit extends Cubit<AuthState> {
   final authRemoteRepository = AuthRemoteRepository();
+  final authLocalRepository = AuthLocalRepository();
   final spService = SpService();
   void getUserData() async{
     try {
       emit(AuthUserLoading());
     final userModel = await authRemoteRepository.getUserData();
+    print(userModel);
     if(userModel != null){
       emit(AuthLoggedIn(userModel));
     }else{
@@ -48,8 +51,8 @@ class AuthCubit extends Cubit<AuthState> {
     final  userModel = await authRemoteRepository.logIn( email: email, password: password);
     if(userModel.token!.isNotEmpty){
       spService.setToken(userModel.token as String);
-    }
-     
+    } 
+     await authLocalRepository.insertUser(userModel);
     emit(AuthLoggedIn(userModel));
     } catch (e) {
       emit(AuthError(e.toString()));
