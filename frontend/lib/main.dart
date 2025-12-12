419d2ac2f6ad11477e074e84e74a1ba91f6e5bdc
Samuel Ketema <samuelketema01@gmail.com>
2025-12-12 21:02:55 +0300

    state management for root widget.

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -5,53 +5,67 @@ import 'package:taskapp/features/auth/pages/home_page.dart';
 import 'package:taskapp/features/auth/pages/signup_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    MultiBlocProvider(
+      providers: [BlocProvider(create: (_) => AuthCubit())],
+      child: const MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [BlocProvider(create: (_) => AuthCubit())],
-      child: MaterialApp(
-        title: 'Task App',
-        theme: ThemeData(
-          elevatedButtonTheme: ElevatedButtonThemeData(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.black,
-              minimumSize: Size(double.infinity, 66),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-            ),
-          ),
-          inputDecorationTheme: InputDecorationTheme(
-            contentPadding: EdgeInsets.all(27),
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10),
-              borderSide: BorderSide(color: Colors.grey.shade300, width: 3),
-            ),
-            errorBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10),
-              borderSide: BorderSide(color: Colors.red, width: 3),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10),
-              borderSide: BorderSide(width: 3),
-            ),
-          ),
-        ),
+  State<MyApp> createState() => _MyAppState();
+}
 
-        home: BlocBuilder<AuthCubit, AuthState>(
-          builder: (context, state) {
-            if (state is AuthLoggedIn) {
-              return HomePage();
-            }
-            return SignupPage();
-          },
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<AuthCubit>().getUserData();
+
+  }
+
+  @override 
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Task App',
+      theme: ThemeData(
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.black,
+            minimumSize: Size(double.infinity, 66),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
         ),
+        inputDecorationTheme: InputDecorationTheme(
+          contentPadding: EdgeInsets.all(27),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(color: Colors.grey.shade300, width: 3),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(color: Colors.red, width: 3),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+            borderSide: BorderSide(width: 3),
+          ),
+        ),
+      ),
+
+      home: BlocBuilder<AuthCubit, AuthState>(
+        builder: (context, state) {
+          if (state is AuthLoggedIn) {
+            return HomePage();
+          }
+          return SignupPage();
+        },
       ),
     );
   }
