75ae982f185e32f6afd85db1c3c0da89f7b958e3
Samuel Ketema <samuelketema01@gmail.com>
2025-12-15 22:45:25 +0300

    Date selector added.

--- frontend/lib/core/constants/utils.dart
+++ frontend/lib/core/constants/utils.dart
@@ -7,3 +7,10 @@ Color strengthenColor(Color color, double factor) {
 
   return Color.fromARGB(color.alpha, r, g, b);
 }
+
+List<DateTime> generateWeekDate(int weekOffset){
+  DateTime today = DateTime.now();
+  DateTime startOfWeek = today.subtract(Duration(days:today.weekday ));
+   startOfWeek = startOfWeek.add(Duration(days: weekOffset * 7));
+   return List.generate(7, (index)=> startOfWeek.add(Duration(days: index)));
+}
