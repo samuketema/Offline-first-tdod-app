8682a8ca679a2a1c9b48c03d29fa2596a302a3aa
Samuel Ketema <samuelketema01@gmail.com>
2025-12-15 22:46:22 +0300

    new class for the date selector.

---
+++ frontend/lib/features/auth/widgets/date_selector.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:taskapp/core/constants/utils.dart';
+
+class DateSelector extends StatefulWidget {
+  const DateSelector({super.key});
+
+  @override
+  State<DateSelector> createState() => _DateSelectorState();
+}
+
+class _DateSelectorState extends State<DateSelector> {
+  int weekOffset = 0;
+  DateTime selectedDate = DateTime.now();
+  @override
+  Widget build(BuildContext context) {
+    final weekDates = generateWeekDate(weekOffset);
+    final currentMonth = DateFormat("MMMM").format(weekDates.first);
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10).copyWith(bottom: 10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    weekOffset++;
+                  });
+                },
+                icon: Icon(Icons.arrow_back_ios),
+              ),
+              Text(
+                currentMonth,
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    weekOffset--;
+                  });
+                },
+                icon: Icon(Icons.arrow_forward_ios),
+              ),
+            ],
+          ),
+        ),
+        SizedBox(
+          height: 60,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: weekDates.length,
+            itemBuilder: (context, index) {
+              final date = weekDates[index];
+              bool isSelected = date.year == selectedDate.year &&
+                  date.month == selectedDate.month &&
+                  date.day == selectedDate.day;
+
+              return Container(
+                decoration: BoxDecoration(
+                  border: BoxBorder.all(color: Colors.grey.shade300),
+                  color:isSelected ? Colors.orange:null,
+                ),
+                width: 55,
+                margin: const EdgeInsets.symmetric(horizontal: 6),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      date.day.toString(),
+                      style: TextStyle(
+                        color:isSelected ? Colors.white:null,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      DateFormat("EEE").format(date),
+                      style:  TextStyle(
+                       color:isSelected ? Colors.white:null,  
+                      fontSize: 12),
+                    ),
+                  ],
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
