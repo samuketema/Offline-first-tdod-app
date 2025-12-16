696d6a11039ebb10fbd1cd701b5ac52e5421c8cd
Samuel Ketema <samuelketema01@gmail.com>
2025-12-16 23:21:23 +0300

    New page for adding task

---
+++ frontend/lib/features/auth/pages/add_new_task.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class AddNewTask extends StatefulWidget {
+  const AddNewTask({super.key});
+
+  @override
+  State<AddNewTask> createState() => _AddNewTaskState();
+}
+
+class _AddNewTaskState extends State<AddNewTask> {
+  DateTime selectedDate = DateTime.now();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add New Task"),
+        actions: [
+          GestureDetector(
+            onTap: () async{
+              final _selectedDate = await showDatePicker(context: context, firstDate: DateTime.now(), lastDate: DateTime.now().add(Duration(days: 90)));
+              if (_selectedDate != null) {
+               setState(() {
+                  selectedDate = _selectedDate;
+               });
+              }
+            },
+            child: Text(DateFormat("MM-D-Y ").format(selectedDate)),
+          )
+        ],
+      ),
+    );
+  }
+}
