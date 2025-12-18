d673230839e54ddcffb55a407a7c0f3a0413404f
Samuel Ketema <samuelketema01@gmail.com>
2025-12-18 23:15:42 +0300

    debugging for task.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,13 @@
 import express   from "express";
 import cors from 'cors';
 import authRouter from "./routes/auth";
+import taskRouter from "./routes/task";
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/auth",authRouter);
-
+app.use("/tasks" , taskRouter);
 
 
 app.get('/' ,(req,res)=>{
