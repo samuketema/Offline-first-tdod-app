974e4cb481ddbe150c986626c84bc1efa73df892
Samuel Ketema <samuelketema01@gmail.com>
2025-12-18 23:16:55 +0300

    New cubit for task state management.

---
+++ frontend/lib/features/auth/home/cubit/task_cubit.dart
@@ -0,0 +1,34 @@
+  import 'package:flutter/widgets.dart';
+  import 'package:flutter_bloc/flutter_bloc.dart';
+  import 'package:taskapp/core/constants/utils.dart';
+  import 'package:taskapp/features/auth/home/repositories/task_remote_repository.dart';
+  import 'package:taskapp/models/task_model.dart';
+  part 'task_state.dart';
+  class TaskCubit extends Cubit<TaskState> {
+    TaskCubit() : super(TaskInitial());
+    final  repository = TaskRemoteRepository(); 
+
+    Future<void> createTask({
+      required String title,
+      required String description,
+      required Color color,
+      required String token,
+      required DateTime dueAt, 
+    }) async {
+      try {
+        emit(TaskLoading());
+
+        final task = await repository.createTask(
+          title: title,
+          description: description,
+          hexColor: rgbToHex(color),
+          token: token, 
+          dueAt: dueAt
+        );
+
+        emit(TaskSuccess(task));
+      } catch (e) {
+        emit(TaskFailure(e.toString()));
+      }
+    }
+  }
---
+++ frontend/lib/features/auth/home/cubit/task_state.dart
@@ -0,0 +1,23 @@
+part of "task_cubit.dart";
+
+sealed class TaskState {
+  const TaskState();
+}
+
+final class TaskInitial extends TaskState {
+  const TaskInitial();
+}
+
+final class TaskLoading extends TaskState {
+  const TaskLoading();
+}
+
+final class TaskSuccess extends TaskState {
+  final TaskModel task;
+  const TaskSuccess(this.task);
+}
+
+final class TaskFailure extends TaskState {
+  final String message;
+  const TaskFailure(this.message);
+}
