63412614d51cfd7a42837ef2b703425d45069e5b
Samuel Ketema <samuelketema01@gmail.com>
2025-12-25 21:49:57 +0300

    creates and runs an Express server on port 8000

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,17 @@
-import express   from "express";
-import cors from 'cors';
+import express from "express";
 import authRouter from "./routes/auth";
 import taskRouter from "./routes/task";
+
 const app = express();
 
-app.use(cors());
 app.use(express.json());
-app.use("/auth",authRouter);
-app.use("/tasks" , taskRouter);
+app.use("/auth", authRouter);
+app.use("/tasks", taskRouter);
 
-
-app.get('/' ,(req,res)=>{
-    res.send("Our application is RUNNING SMOOTHLY");
+app.get("/", (req, res) => {
+  res.send("Welcome to my app!!!!!!!");
 });
 
-
-app.listen(8000,()=>{
-    console.log(`Server running on port 8000`);
-
-})
+app.listen(8000, () => {
+  console.log("Server started on port 8000");
+});
