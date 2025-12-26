64d9473f70ae01eeaddde7421eeae4c9801d17f2
Samuel Ketema <samuelketema01@gmail.com>
2025-12-26 22:10:13 +0300

    State management for authentication

--- frontend/lib/features/auth/cubit/auth_cubit.dart
+++ frontend/lib/features/auth/cubit/auth_cubit.dart
@@ -1,59 +1,70 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:taskapp/core/services/sp_service.dart';
-import 'package:taskapp/features/auth/repositories/auth_local_repository.dart';
-import 'package:taskapp/features/auth/repositories/auth_remote_repository.dart';
-import 'package:taskapp/models/user_model.dart';
+import 'package:frontend/core/services/sp_service.dart';
+import 'package:frontend/features/auth/repository/auth_local_repository.dart';
+import 'package:frontend/features/auth/repository/auth_remote_repository.dart';
+import 'package:frontend/models/user_model.dart';
+
 part 'auth_state.dart';
 
 class AuthCubit extends Cubit<AuthState> {
+  AuthCubit() : super(AuthInitial());
   final authRemoteRepository = AuthRemoteRepository();
   final authLocalRepository = AuthLocalRepository();
   final spService = SpService();
-  void getUserData() async{
+
+  void getUserData() async {
     try {
-      emit(AuthUserLoading());
-    final userModel = await authRemoteRepository.getUserData();
-    print(userModel);
-    if(userModel != null){
-      emit(AuthLoggedIn(userModel));
-    }else{
+      emit(AuthLoading());
+      final userModel = await authRemoteRepository.getUserData();
+      if (userModel != null) {
+        await authLocalRepository.insertUser(userModel);
+        emit(AuthLoggedIn(userModel));
+      } else {
+        emit(AuthInitial());
+      }
+    } catch (e) {
+      print(e);
       emit(AuthInitial());
     }
-    
-    } catch (e) {
-      emit(AuthError(e.toString()));
-    }
   }
-  AuthCubit():super(AuthInitial());
 
   void signUp({
     required String name,
     required String email,
     required String password,
-  })async {
+  }) async {
     try {
-    
-      emit(AuthUserLoading());
-    await authRemoteRepository.signUp(name: name, email: email, password: password);
-    emit(AuthSignUp());
+      emit(AuthLoading());
+      await authRemoteRepository.signUp(
+        name: name,
+        email: email,
+        password: password,
+      );
+
+      emit(AuthSignUp());
     } catch (e) {
       emit(AuthError(e.toString()));
     }
   }
 
-  void logIn({
+  void login({
     required String email,
-    required String password
-  })async{
-     try {
-     
-      emit(AuthUserLoading());
-    final  userModel = await authRemoteRepository.logIn( email: email, password: password);
-    if(userModel.token!.isNotEmpty){
-      spService.setToken(userModel.token as String);
-    } 
-     await authLocalRepository.insertUser(userModel);
-    emit(AuthLoggedIn(userModel));
+    required String password,
+  }) async {
+    try {
+      emit(AuthLoading());
+      final userModel = await authRemoteRepository.login(
+        email: email,
+        password: password,
+      );
+
+      if (userModel.token.isNotEmpty) {
+        await spService.setToken(userModel.token);
+      }
+
+      await authLocalRepository.insertUser(userModel);
+
+      emit(AuthLoggedIn(userModel));
     } catch (e) {
       emit(AuthError(e.toString()));
     }
--- frontend/lib/features/auth/cubit/auth_state.dart
+++ frontend/lib/features/auth/cubit/auth_state.dart
@@ -1,18 +1,19 @@
-part of 'auth_cubit.dart';
+part of "auth_cubit.dart";
 
 sealed class AuthState {}
 
-final class AuthInitial extends AuthState{}
-final class AuthLoggedIn extends AuthState{
+final class AuthInitial extends AuthState {}
+
+final class AuthLoading extends AuthState {}
+
+final class AuthSignUp extends AuthState {}
+
+final class AuthLoggedIn extends AuthState {
   final UserModel user;
-
   AuthLoggedIn(this.user);
-
 }
-final class AuthUserLoading extends AuthState{}
-final class AuthSignUp extends AuthState{}
-final class AuthError extends AuthState{
-  final String error;
 
-  AuthError( this.error);
+final class AuthError extends AuthState {
+  final String error;
+  AuthError(this.error);
 }
