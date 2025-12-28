92d7abcc5f1ecb68292eeb6f905c81acf5e6b55a
Samuel Ketema <samuelketema01@gmail.com>
2025-12-28 21:57:05 +0300

    login_page

--- frontend/lib/features/auth/pages/login_page.dart
+++ frontend/lib/features/auth/pages/login_page.dart
@@ -1,10 +1,13 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:taskapp/features/auth/cubit/auth_cubit.dart';
-import 'package:taskapp/features/auth/pages/home_page.dart';
+import 'package:frontend/features/auth/cubit/auth_cubit.dart';
+import 'package:frontend/features/auth/pages/signup_page.dart';
+import 'package:frontend/features/home/pages/home_page.dart';
 
 class LoginPage extends StatefulWidget {
+  static MaterialPageRoute route() => MaterialPageRoute(
+        builder: (context) => const LoginPage(),
+      );
   const LoginPage({super.key});
 
   @override
@@ -16,94 +19,128 @@ class _LoginPageState extends State<LoginPage> {
   final passwordController = TextEditingController();
   final formKey = GlobalKey<FormState>();
 
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   void loginUser() {
     if (formKey.currentState!.validate()) {
-      context.read<AuthCubit>().logIn(email: emailController.text.trim(), password: passwordController.text.trim());
-     
+      context.read<AuthCubit>().login(
+            email: emailController.text.trim(),
+            password: passwordController.text.trim(),
+          );
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocConsumer<AuthCubit, AuthState>(
-        listener: (context, state) {
-          if (state is AuthError ) {
-          ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(state.error)));
-          }else if(state is AuthLoggedIn){
-            Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (_)=>HomePage()), (Route<dynamic> route) => false );
-          }
-        },
-        builder: (context, state) {
-          if (state is AuthLoggedIn) {
-            return Center( child:  CircularProgressIndicator(),);
-          }
-          return Padding(
-            padding: const EdgeInsets.all(16),
-            child: Form(
-              key: formKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Text(
-                    "Login",
-                    style: TextStyle(fontSize: 55, fontWeight: FontWeight.bold),
+        body: BlocConsumer<AuthCubit, AuthState>(
+      listener: (context, state) {
+        if (state is AuthError) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(state.error),
+            ),
+          );
+        } else if (state is AuthLoggedIn) {
+          Navigator.pushAndRemoveUntil(
+            context,
+            HomePage.route(),
+            (_) => false,
+          );
+        }
+      },
+      builder: (context, state) {
+        if (state is AuthLoading) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        return Padding(
+          padding: const EdgeInsets.all(15.0),
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  "Login.",
+                  style: TextStyle(
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
                   ),
-                  SizedBox(height: 30),
-                  TextFormField(
-                    controller: emailController,
-                    decoration: InputDecoration(hintText: "Email"),
-                    validator: (value) {
-                      if (value == null || value.trim().isEmpty) {
-                        return "Email cannot be empty";
-                      }
-                      return null;
-                    },
+                ),
+                const SizedBox(height: 30),
+                TextFormField(
+                  controller: emailController,
+                  decoration: const InputDecoration(
+                    hintText: 'Email',
                   ),
-                  SizedBox(height: 15),
-                  TextFormField(
-                    controller: passwordController,
-                    decoration: InputDecoration(hintText: "Password"),
-                    obscureText: true,
-                    validator: (value) {
-                      if (value == null || value.trim().isEmpty) {
-                        return "Password cannot be empty";
-                      }
-                      return null;
-                    },
+                  validator: (value) {
+                    if (value == null ||
+                        value.trim().isEmpty ||
+                        !value.trim().contains("@")) {
+                      return "Email field is invalid!";
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 15),
+                TextFormField(
+                  controller: passwordController,
+                  decoration: const InputDecoration(
+                    hintText: 'Password',
                   ),
-                  SizedBox(height: 15),
-                  ElevatedButton(
-                    onPressed: loginUser,
-                    child: Text(
-                      "Login",
-                      style: TextStyle(fontSize: 15, color: Colors.white),
+                  validator: (value) {
+                    if (value == null ||
+                        value.trim().isEmpty ||
+                        value.trim().length <= 6) {
+                      return "Password field is invalid!";
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: loginUser,
+                  child: const Text(
+                    'LOGIN',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
                     ),
                   ),
-                  SizedBox(height: 15),
-                  RichText(
+                ),
+                const SizedBox(height: 20),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(SignupPage.route());
+                  },
+                  child: RichText(
                     text: TextSpan(
-                      text: "Don't have an account? ",
+                      text: 'Don\'t have an account? ',
                       style: Theme.of(context).textTheme.titleMedium,
-                      children: [
+                      children: const [
                         TextSpan(
-                          text: 'Sign Up.',
-                          style: TextStyle(color: Colors.blue),
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () {
-                              // Navigate back to signup
-                              Navigator.pop(context);
-                            },
+                          text: 'Sign Up',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ],
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          );
-        },
-      ),
-    );
+          ),
+        );
+      },
+    ));
   }
 }
