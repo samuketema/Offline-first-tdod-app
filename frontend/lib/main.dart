5b1f4533cd4f7d46952f302a1c0e03a4dbb05c7b
Samuel Ketema <samuelketema01@gmail.com>
2025-12-30 21:33:28 +0300

    Main

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:taskapp/features/auth/cubit/auth_cubit.dart';
-import 'package:taskapp/features/auth/home/cubit/task_cubit.dart';
-import 'package:taskapp/features/auth/pages/home_page.dart';
-import 'package:taskapp/features/auth/pages/signup_page.dart';
+import 'package:frontend/features/auth/cubit/auth_cubit.dart';
+import 'package:frontend/features/auth/pages/signup_page.dart';
+import 'package:frontend/features/home/cubit/tasks_cubit.dart';
+import 'package:frontend/features/home/pages/home_page.dart';
 
 void main() {
   runApp(
     MultiBlocProvider(
-      providers: [BlocProvider(create: (_) => AuthCubit()),
-      BlocProvider(create: (_) => TaskCubit()),
-      
+      providers: [
+        BlocProvider(create: (_) => AuthCubit()),
+        BlocProvider(create: (_) => TasksCubit()),
       ],
       child: const MyApp(),
     ),
@@ -29,46 +29,60 @@ class _MyAppState extends State<MyApp> {
   void initState() {
     super.initState();
     context.read<AuthCubit>().getUserData();
-
   }
 
-  @override 
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Task App',
       theme: ThemeData(
+        fontFamily: "Cera Pro",
+        inputDecorationTheme: InputDecorationTheme(
+          contentPadding: const EdgeInsets.all(27),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: Colors.grey.shade300,
+              width: 3,
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: const BorderSide(
+              width: 3,
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          border: OutlineInputBorder(
+            borderSide: const BorderSide(
+              width: 3,
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderSide: const BorderSide(
+              color: Colors.red,
+              width: 3,
+            ),
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
             backgroundColor: Colors.black,
-            minimumSize: Size(double.infinity, 66),
+            minimumSize: const Size(double.infinity, 60),
             shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
+              borderRadius: BorderRadius.circular(15),
             ),
           ),
         ),
-        inputDecorationTheme: InputDecorationTheme(
-          contentPadding: EdgeInsets.all(27),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(color: Colors.grey.shade300, width: 3),
-          ),
-          errorBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(color: Colors.red, width: 3),
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(10),
-            borderSide: BorderSide(width: 3),
-          ),
-        ),
+        useMaterial3: true,
       ),
-
       home: BlocBuilder<AuthCubit, AuthState>(
         builder: (context, state) {
           if (state is AuthLoggedIn) {
-            return HomePage();
+            return const HomePage();
           }
-          return SignupPage();
+          return const SignupPage();
         },
       ),
     );
